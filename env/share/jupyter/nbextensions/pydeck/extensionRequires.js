f3bd0ecbaab4def780f12cf9d531ecde31940149
Ritesh <riteshmaster121@gmail.com>
2024-07-14 17:32:13 -0400

    Initial commit

--- env/share/jupyter/nbextensions/pydeck/extensionRequires.js
+++ env/share/jupyter/nbextensions/pydeck/extensionRequires.js
@@ -1,15 +1,3 @@
-/* eslint-disable */
-define(function() {
-  'use strict';
-  requirejs.config({
-    map: {
-      '*': {
-        '@deck.gl/jupyter-widget': 'nbextensions/pydeck/index'
-      }
-    }
-  });
-  // Export the required load_ipython_extension function
-  return {
-    load_ipython_extension: function() {}
-  };
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4aa244bd6575b6a98eff4ddfdecadfb5942b5286344050256469c194a2c1027
+size 304
